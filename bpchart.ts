d43fc693c1c400a4500bd07b149591db36707e4f
sihu song <navelist89@gmail.com>
2019-11-01 15:36:17 +0900

    moving bpchart

---
+++ bpchart.ts
@@ -0,0 +1,132 @@
+
+import * as moment from 'moment'
+import MomentTimeZone from "moment-timezone";
+
+window['moment'] = moment
+MomentTimeZone();
+
+import * as Highcharts from 'highcharts';
+import Boost from 'highcharts/modules/boost';
+import noData from 'highcharts/modules/no-data-to-display';
+import More from 'highcharts/highcharts-more';
+import { BloodpressureMeasurement } from '@AutochekCommon/vanilla/objects/device-data-object';
+
+Boost(Highcharts);
+noData(Highcharts);
+More(Highcharts);
+noData(Highcharts);
+
+
+
+export function drawBloodpressurePeriodChart(canvas: string, data:BloodpressureMeasurement[], start?: Date, end?: Date) {
+  const bpData = data; 
+  const option = setBloodPressureOption(bpData, start, end);
+  Highcharts.chart(canvas, option);
+}
+  
+  
+
+function setBloodPressureOption(bpData: any, start?:Date, end?:Date) {
+  const options: any = {
+    title: {
+    text: 'Blood Pressure chart'
+    },
+    credits: {
+    enabled: false
+    },
+    tooltip: {
+    shared: true
+    },
+    time: {
+    timezone: 'Asia/Seoul'
+    },
+    xAxis: {
+    type: 'datetime',
+    gridLineWidth: 1,
+    startOnTick: true,
+    endOnTick: true,
+    title: {
+        text: '시간'
+    }
+    },
+    yAxis: [{
+    labels: {
+        format: '{value}mmHg',
+        style: {
+        color: Highcharts.getOptions().colors[3]
+        }
+    }
+    }, {
+    gridLineWidth: 1,
+    opposite: true,
+    title: {
+        text: '맥박',
+    },
+    labels: {
+        format: '{value}회/분',
+        align: 'left',
+    }
+    }],
+    plotOptions: {
+    series: {
+        marker: {
+        enabled: true
+        },
+        pointWidth: 30
+    }
+    },
+    series: [{
+    name: '수축기',
+    type: 'arearange',
+    lineWidth: 1,
+    fillOpacity: 0.3,
+    zIndex: 0,
+    color: Highcharts.getOptions().colors[3],
+    data: []
+    }, {
+    name: '평균 혈압',
+    zIndex: 1,
+    data: [],
+    }, {
+    name: '심박수',
+    type: 'column',
+    yAxis: 1,
+    zIndex: 0,
+    opacity: 0.8,
+    data: []
+    }]
+};
+
+const updatedSystolic = [];
+const updatedAverage = [];
+const updatedRate = [];
+
+bpData.forEach(data => {
+    const time = new Date(data.date).getTime();
+    const tempSystolic = [
+    time, data.diastolic, data.systolic
+    ];
+    const tempAverage = [
+    time, data.mean
+    ];
+    const tempRate = [
+    time, data.rate
+    ];
+    updatedSystolic.push(tempSystolic);
+    updatedAverage.push(tempAverage);
+    updatedRate.push(tempRate);
+});
+
+options.series[0].data = updatedSystolic;
+options.series[1].data = updatedAverage;
+    options.series[2].data = updatedRate;
+    
+    if (start) {
+    options.xAxis.min = start.getTime();
+}
+if (end) {
+    options.xAxis.max = end.getTime();
+}
+
+return options;
+}
